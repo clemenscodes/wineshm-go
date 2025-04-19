efb093d06c315a440c7adaf47496360257ba5f76
Clemens Horn <clemens.horn@mni.thm.de>
2025-04-19 11:23:06 +0200

    feat(nix): add devshell

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    packages = {
+      ${system} = {};
+    };
+    devShells = {
+      ${system} = {
+        default = pkgs.mkShellNoCC {
+          buildInputs = with pkgs; [
+            gcc
+            gnumake
+            go
+            go-bindata
+            wineWow64Packages.stagingFull
+            pkgsCross.mingw32.stdenv.cc
+          ];
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+          shellHook = ''
+
+          '';
+        };
+      };
+    };
+  };
+}
